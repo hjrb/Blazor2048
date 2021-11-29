8a4c7135831b05ab196c68586ee63046480a4bb7
Reister Hansjoerg <ret2fe@bosch.com>
2021-11-29 13:25:05 +0100

    added touch support

--- Blazor2048/wwwroot/site.js
+++ Blazor2048/wwwroot/site.js
@@ -2,23 +2,7 @@
 var serializeEvent = function (e) {
     if (e) {
         let o = {
-            altKey: e.altKey,
-            button: e.button,
-            buttons: e.buttons,
             code: e.code,
-            clientX: e.clientX,
-            clientY: e.clientY,
-            ctrlKey: e.ctrlKey,
-            metaKey: e.metaKey,
-            movementX: e.movementX,
-            movementY: e.movementY,
-            offsetX: e.offsetX,
-            offsetY: e.offsetY,
-            pageX: e.pageX,
-            pageY: e.pageY,
-            screenX: e.screenX,
-            screenY: e.screenY,
-            shiftKey: e.shiftKey
         };
         return o;
     }
@@ -33,7 +17,64 @@ function RegisterKeyDownEventHandler() {
     }, false);
 }
 
+var SwipeStartX = null;
+var SwipeStartY = null;
+var SwipeWidth = null;
+var SwipeArea = null;
+
+function RegisterSwipe() {
+    console.log("RegisterSwipe");
+    const SwipeWidthMax = 100;
+    SwipeArea = document; //.querySelector("main");
+    console.log(SwipeArea);
+    SwipeWidth = Math.min(window.innerWidth * 0.2, SwipeWidthMax);
+    console.log("SwipeWidth: ", SwipeWidth);
+    SwipeArea.addEventListener("touchstart", SwipeStart, false);
+    SwipeArea.addEventListener("touchend", SwipeEnd, false);
+}
+
+function SwipeStart(evt) {
+    console.log("SwipeStart");
+    SwipeStartX = evt.changedTouches[0].clientX;
+    SwipeStartY = evt.changedTouches[0].clientY;
+};
+
+function SwipeEnd(evt) {
+    console.log("SwipeEnd");
+    const SwipeEndX = evt.changedTouches[0].clientX;
+    const SwipeEndY = evt.changedTouches[0].clientY;
+
+    const SwipeLengthX = SwipeStartX - SwipeEndX;
+    const SwipeLengthY = SwipeStartY - SwipeEndY;
+
+    if (Math.abs(SwipeLengthX) > Math.abs(SwipeLengthY)) {
+        if (SwipeLengthX > SwipeWidth) {
+            /* left swipe */
+            console.log("Swipped left");
+            DotNet.invokeMethodAsync('Blazor2048', 'HandleKeyPress', { Code: "ArrowLeft" });
+        }
+        else if (SwipeLengthX < -SwipeWidth) {
+            /* right swipe */
+            console.log("Swipped right");
+            DotNet.invokeMethodAsync('Blazor2048', 'HandleKeyPress', { Code: "ArrowRight" });
+        }
+    }
+    else {
+        if (SwipeLengthY > SwipeWidth) {
+            /* down up */
+            console.log("Swipped up");
+            DotNet.invokeMethodAsync('Blazor2048', 'HandleKeyPress',  { Code: "ArrowUp" });
+        }
+        else if (SwipeLengthY < -SwipeWidth) {
+            /* up down */
+            console.log("Swipped down");
+            DotNet.invokeMethodAsync('Blazor2048', 'HandleKeyPress',  { Code: "ArrowDown" });
+        }
+    }
+}
+
 // register a document event hanlder for key down
 document.addEventListener("DOMContentLoaded", function (event) {
     RegisterKeyDownEventHandler();
+    RegisterSwipe();
 });
